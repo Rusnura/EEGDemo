fa9532cc852995234f3bac4ee084ec848850959b
Ruslan Tumasoff <Rus-nura@yandex.ru>
2024-04-20 10:01:16 +0700

    develop sample application for neurosky sdk

--- EEGApplication/enums/NS_ALGO_BCQ_VALID_TYPE.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EEGApplication.enums
-{
-    public enum NS_ALGO_BCQ_VALID_TYPE
-    {
-        NS_ALGO_BCQ_TYPE_VALUE = 1,
-        NS_ALGO_BCQ_TYPE_VALID = 2,
-        NS_ALGO_BCQ_TYPE_BOTH = 3
-    }
-}
